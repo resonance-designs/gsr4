b9a04697806326c563476fe69b9e8e09893c31b8
Richard Bakos <resonance.designs.com@gmail.com>
2026-02-05 02:39:18 -0500

    update: removed .docusaurus from doc-site

--- docs-site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Dev\\Projects\\grainrust\\docs-site\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Dev\\Projects\\grainrust\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Dev\\Projects\\grainrust\\docs-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Dev\\Projects\\grainrust\\docs-site\\src\\css\\custom.css"),
-];
